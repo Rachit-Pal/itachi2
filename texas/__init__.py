53dbcfb32f7678f52dd7b83606585f8279307369
Chizuru <chizuru@kanojo.tk>
2021-03-11 10:28:01 +0530

    use memorystorage

--- texas/__init__.py
+++ texas/__init__.py
@@ -19,7 +19,7 @@ import spamwatch
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.bot.api import TelegramAPIServer, TELEGRAM_PRODUCTION
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from texas.config import get_str_key, get_int_key, get_list_key, get_bool_key
 from texas.utils.logger import log
@@ -54,11 +54,7 @@ else:
 
 # AIOGram
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML, server=server)
-storage = RedisStorage2(
-    host=get_str_key("REDIS_URI"),
-    port=get_int_key("REDIS_PORT"),
-    password=get_str_key("REDIS_PASS")
-)
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 loop = asyncio.get_event_loop()
